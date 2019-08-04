235b333458d43d4ec410a2cf0b5e61e03988901d
voidstar <drif7walk@gmail.com>
2019-08-04 02:46:59 -0700

    Update 8ch.py

--- 8ch.py
+++ 8ch.py
@@ -3,6 +3,8 @@ import requests
 import random 
 import time
 from fake_useragent import UserAgent
+import os, random
+
 session = requests.session()
 session.proxies = {}
 session.proxies['http'] = 'socks5h://localhost:9050'
@@ -15,41 +17,29 @@ print("Started...")
 
 while True:
 	boards = [
-		"cow",
-		"b2",
-		"b",
-		"pol",
-		"soc",
 		"v",
-		"tv",
-		"dempart",
-		"hgg",
-		"choroy",
 		"vichan",
-		"a",
-		"vg",
+		"b2",
+		"leftypol",
+		"x",
+		"pol",
+		"tv",
 		"tech",
-		"christian",
-		"k",
-		"newsplus",
-		"tg",
-		"abdl",
-		"delicious",
-		"monster",
-		"animu",
-		"fur",
+		"a",
+		"cow",
+		"hgg",
 		"r9k",
-		"abc",
-		"dempart",
-		"freeb",
-		"sissy",
-		"trap",
+		"tg",
+		"fur",
+		"vg",
+		"b",
+		"newsplus"
 	]
 	b = random.choice(boards)
 	board = py8chan.Board(b)
 	threads = board.get_all_thread_ids()
 	#t = random.choice(threads)
-	t = threads[random.randint(5,10)]
+	t = threads[random.randint(5,20)]
 	
 	msgs = [
 		"suicide in 20 minutes gg/Xq2uYaa",
@@ -62,30 +52,33 @@ while True:
 		"NO CP ALLOWED gg/Xq2uYaa",
 		"join to see self harm gg/Xq2uYaa",
 		"Official 8chan + 4chan Discord server gg/Xq2uYaa",
-		"FREE STEAM GIVEAWAY gg/Xq2uYaa"
+		"FREE STEAM GIVEAWAY gg/Xq2uYaa",
 		"FREE PREMIUM SNAP GIVEAWAY gg/Xq2uYaa",
-		"suicide youtube stream in 20 minutes gg/Xq2uYaa"
+		"suicide youtube stream in 20 minutes gg/Xq2uYaa",
 		"Awesome server for Anonymous, please join! gg/Xq2uYaa",
-		"gg/Xq2uYaa gg/Xq2uYaa gg/Xq2uYaa gg/Xq2uYaa"
-		"Furry server let's go raid! gg/Xq2uYaa"
-	]
-	
-	msgs = [
+		"gg/Xq2uYaa gg/Xq2uYaa gg/Xq2uYaa gg/Xq2uYaa",
+		"Furry server let's go raid! gg/Xq2uYaa",
 		"Anonymous server with self-harm, traps, steam giveaways, premium snaps and minimal rules! gg/Xq2uYaa"
 	]
 	msg = random.choice(msgs)
+	
+	images = "anime_girls/"
+	image_path = images + random.choice(os.listdir(images))
+	image_filename = os.path.basename(image_path)
+	file = (image_filename, open(image_path, 'rb'))
+	file = ""
 	multipart_form_data = {
 		#'file2': ('custom_file_name.zip', open('myfile.zip', 'rb')),
 		#'action': (None, 'store'),
 		'thread': (None,  t),
 		'board': (None, b),
 		'name': (None, ''),
-		'body': (None, str(random.randint(1000,9999)) + msg),
+		'body': (None, str(random.randint(1000,9999)) + " " + msg),
 		'password': (None, 'password'),
 		'user_flag': (None, ''),
 		'json_response': (None, '1'),
 		'post': (None, 'New Reply'),
-		'file': (None, 'undefined'),
+		'file': file,
 		'file2': (None, 'undefined'),
 		'file3': (None, 'undefined'),
 		'file4': (None, 'undefined'),
@@ -97,4 +90,4 @@ while True:
 	}
 	response = requests.post('https://sys.8ch.net/post.php', files=multipart_form_data, headers=headers)
 	print("POSTING IN " + b + ": " + str(response.content))
-	time.sleep(random.randint(15,30))
+	time.sleep(random.randint(5,20))
